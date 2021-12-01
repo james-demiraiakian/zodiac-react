a35ff8fb388fad0cb6790361140fb0ce51b9326b
James Demiraiakian <james.armen@gmail.com>
2021-11-30 17:00:29 -0800

    functionally done. Stylistically miles off

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -4,15 +4,14 @@ import './Main.css'
 
 import { zodiacs } from '../../data.js'
 import Zodiac from '../Zodiac/Zodiac.js'
-// import bagr from '../../background.jpg'
+import bagr from '../../background.jpg'
 
 export default function Main() {
-  console.log('stuff')
   return (
-    <main /*style={{ backgroundImage: `url(${bagr})` }}*/>
-      {zodiacs.map((zodiac) => {
-        ;<Zodiac key={zodiac.name} {...zodiac} />
-      })}
+    <main style={{ backgroundImage: `url(${bagr})` }}>
+      {zodiacs.map((zodiac) => (
+        <Zodiac key={zodiac.name} {...zodiac} />
+      ))}
     </main>
   )
 }
--- src/components/Zodiac/Zodiac.js
+++ src/components/Zodiac/Zodiac.js
@@ -2,12 +2,12 @@ import React from 'react'
 
 import './Zodiac.css'
 
-export default function Zodiac(props) {
+export default function Zodiac({ name, dates }) {
   return (
     <div className="zodiac">
-      <img height="75" src={`${process.env.PUBLIC_URL}/zodiac-imgs/${props.name}.svg`} />
-      <span className="name">{props.name}</span>
-      <span className="dates">{props.dates}</span>
+      <img alt={name} height="75" src={`${process.env.PUBLIC_URL}/zodiac-imgs/${name}.png`} />
+      <span className="name">{name}</span>
+      <span className="dates">{dates}</span>
     </div>
   )
 }
